5490a460b84661bc2ef8320a99c8ad80f11ca450
Kate Alieksiieva <116977603+alekskate11@users.noreply.github.com>
2024-03-20 23:34:47 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,31 @@
+const Orders=[
+	{
+		productName: 'Ruko Fu Pro Drone', 
+		productNumber: '49347', 
+		paymentStatus: 'Due', 
+		shipping: 'Pending'
+	},
+	{
+		productName: 'Ruko Fu Pro Drone', 
+		productNumber: '49347', 
+		paymentStatus: 'Due', 
+		shipping: 'Pending'
+	},
+	{
+		productName: 'Ruko Fu Pro Drone', 
+		productNumber: '49347', 
+		paymentStatus: 'Due', 
+		shipping: 'Declined'
+	},{
+		productName: 'Ruko Fu Pro Drone', 
+		productNumber: '49347', 
+		paymentStatus: 'Due', 
+		shipping: 'Delivered'
+	},
+	{
+		productName: 'Ruko Fu Pro Drone', 
+		productNumber: '49347', 
+		paymentStatus: 'Due', 
+		shipping: 'Pending'
+	}
+]
